3b5839edf9e754f66710f5b88e2b92509a2863ff
Bless12Ahadjie <blaise0ahadjie@gmail.com>
2023-03-18 17:36:18 -0700

    hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/*
+ * this function uses
+ * the puts() to print output instead of 
+ * printf
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts(""Programming is like building a multilingual puzzle");
 	return (0);
 }
