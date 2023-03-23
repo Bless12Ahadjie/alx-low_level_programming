a7273e1d670341a31376a4935fc5494a8edcd52f
Bless12Ahadjie <blaise0ahadjie@gmail.com>
2023-03-22 20:42:56 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int num, num1;
+
+	for (num = 48; num <= 56; num++)
+	{
+		for (num1 = 49; num1 <= 57; num1++)
+		{
+			if (num1 > num)
+			{
+				putchar(num);
+				putchar(num1);
+				if (num != 56 || num1 != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
