005007c3eee3e30290084551f3330934860935f2
Bless12Ahadjie <blaise0ahadjie@gmail.com>
2023-03-25 12:19:55 -0700

    alphabet on ten lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: Always void
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int j = 9;
+
+	while (j <= 9)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+			_putchar('\n');
+		}
+		j++;
+	}
+}
