683a8170481260c78424ae86fb5d1f7ebdb01a03
Bless12Ahadjie <blaise0ahadjie@gmail.com>
2023-03-25 12:01:31 -0700

    printing alphabet using for loop and a function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - This prints alphabet when called in the main 
+ *
+ * Return: Always void (success)
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+	for(alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
