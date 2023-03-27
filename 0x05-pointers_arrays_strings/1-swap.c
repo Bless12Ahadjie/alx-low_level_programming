a333fc521a872e9c0abe235869f4eaf4c76966b9
Bless12Ahadjie <blaise0ahadjie@gmail.com>
2023-03-27 16:11:13 -0700

    swapping values of a parameter

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - this function swaps pointers passed as parameters
+ *
+ * @a: a character to be swapped
+ * @b:a character to be swapped
+ *
+ * Return: Always void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	int c;
+
+	c = *a;
+}
