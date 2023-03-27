1152daa9a1dda0800331d03a336bcb34900c6da9
Bless12Ahadjie <blaise0ahadjie@gmail.com>
2023-03-27 16:19:11 -0700

    print string legth

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of thr string
+ *
+ * @s: character used
+ * Return: Always length of *s
+ */
+int _strlen(char *s)
+{
+		int c = 0;
+
+	while (c >= 0)
+	{
+		if (s[c] == '\0')
+		{
+			break;
+		}
+		else
+		{
+			c++;
+		}
+	}
+	return (c);
+	return (0);
+	
+}
