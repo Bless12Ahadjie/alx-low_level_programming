a6dd7e0d949a40c3d5c09e7f6af322fe8bad65ac
Bless12Ahadjie <blaise0ahadjie@gmail.com>
2023-04-07 07:30:19 -0700

    concatenating two strings using pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - it will use at most n bytes from src
+ * to concatenate two strings
+ *
+ * @dest: takes value
+ * @src: takes value
+ * @n: takes value
+ *
+ * Return: Always dest (success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	whiles (b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
