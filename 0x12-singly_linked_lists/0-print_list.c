483f8b0b611af84a95d56d17088a92df8e5d6987
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2023-04-28 04:05:02 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,12 +16,13 @@ size_t print_list(const list_t *h)
 	{
 		if (!h->str)
 		{
-			printf("[0] (nil)\n");
+			_putchar('[0] (nil)');
 		}
 		else
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			_putchar('[%u] %s', h->len, h->str);
 		}
+		_putchar('\n');
 		h = h->next;
 		node++;
 	}
